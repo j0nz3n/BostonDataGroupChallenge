19f9deac81cf46e21a50e8080bfe8a22793d92c5
j0nathan <jonathan.dejesus85@gmail.com>
2018-09-29 20:58:49 -0400

    Updated app/views/job_postings/index.html.erb to show region for the job posting. A job poster need only add or select the location of the job posting and the region is automatically added on the show page. Because the region is added at render, if the location is updated the region will be updated at render.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
 class Location < ApplicationRecord
   has_many :job_postings, dependent: :destroy
-
+  has_one :region
+  
   validates :name, presence: true
   validates :street_address_1, presence: true
   validates :city, presence: true
